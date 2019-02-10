4fc0e126b8910aea5d33f15998a4b448898f39e5
Junior Sanchez <r.a.s.g65@gmail.com>
2019-02-09 23:06:17 -0400

    add models to sanidad and model to core

--- sanidad/admin.py
+++ sanidad/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from sanidad.models import Account, Company, Driver, Transport
 # Register your models here.
+
+admin.site.register(Account)
+admin.site.register(Company)
+admin.site.register(Driver)
+admin.site.register(Transport)
