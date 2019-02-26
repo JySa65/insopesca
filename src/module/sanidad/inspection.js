e155cc90816db4598f2117283c4236cc75080e63
Junior Sanchez <r.a.s.g65@gmail.com>
2019-02-26 09:17:21 -0400

    create vieew add inspection

--- src/module/sanidad/inspection.js
+++ src/module/sanidad/inspection.js
@@ -1,5 +1,19 @@
-$('select').datepicker({
+$('#id_date').datepicker({
     language: 'es',
     title: "Insopesca",
     autoclose: true,
 });
+
+$('#id_next_date').datepicker({
+    language: 'es',
+    title: "Insopesca",
+    autoclose: true,
+});
+
+const date = document.querySelector("#id_date")
+const next_date = document.querySelector("#id_next_date")
+const result = document.querySelector("#id_result")
+
+if (date && next_date && result) {
+    
+}
