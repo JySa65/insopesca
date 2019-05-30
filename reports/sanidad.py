701824cf9222f635cc664fdfcf17df522261e501
Junior Sanchez <r.a.s.g65@gmail.com>
2019-05-30 13:03:38 -0400

    report pdf 3

--- reports/sanidad.py
+++ reports/sanidad.py
@@ -2,7 +2,7 @@ from django.views.generic import View
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.files.storage import FileSystemStorage
-from datetime import datetime,date
+from datetime import datetime, date
 from reports.base import PDF, FILENAME
 from utils.alert import alert
 from sanidad import models
@@ -107,22 +107,22 @@ class ReportListCompanyOrDriver(View):
         date_2 = self.request.GET.get('date2', '')
         status = self.valid_type(report_select)
         model = models.Company if report_select == "all_company" else models.Driver
-        print(len(date_1),len(date_2))
+        print(len(date_1), len(date_2))
         if len(date_1) == 0 and len(date_2) == 0:
             datas = model.objects.all()
         else:
             f_date1, f_date2 = date_1.replace("/", ""), date_2.replace("/", "")
             day1, month1, year1, day2, month2, year2 = (f_date1[0:2],
-                    f_date1[2:4], f_date1[4:8], f_date2[0:2],
-                        f_date2[2:4], f_date2[4:8])
+                                                        f_date1[2:4], f_date1[4:8], f_date2[0:2],
+                                                        f_date2[2:4], f_date2[4:8])
 
             start, end = (datetime(int(year1), int(month1), int(day1), 0, 0),
-                        datetime(int(year2), int(month2), int(day2), 23, 59))
- 
+                          datetime(int(year2), int(month2), int(day2), 23, 59))
+
             # print(start,end)
             # datas = model.objects.all()
- 
-            datas = model.objects.filter(created_at__range=(start,end))
+
+            datas = model.objects.filter(created_at__range=(start, end))
         return status, datas, model
 
     def get(self, *args, **kwargs):
@@ -142,7 +142,7 @@ class ReportListCompanyOrDriver(View):
         pdf.cell(7.9, 8, '#', 1, 0, 'C')
         pdf.cell(40, 8, 'Documento', 1, 0, 'C')
         (pdf.cell(120, 8, 'Nombre', 1, 0, 'C') if _type == models.Company else
-                        pdf.cell(120, 8, 'Nombres y Apellidos', 1, 0, 'C'))
+         pdf.cell(120, 8, 'Nombres y Apellidos', 1, 0, 'C'))
         pdf.cell(35, 8, 'Telefono Movil', 1, 0, 'C')
         if _type == models.Company:
             pdf.cell(40, 8, 'SPES', 1, 0, 'C')
@@ -198,7 +198,7 @@ class ReportIndividualCompanyOrDriver(View):
 
     def set_data(self):
         report_select = self.request.GET.get('typei', '')
-        ppk = self.request.GET.get('pk','')
+        ppk = self.request.GET.get('pk', '')
         print(report_select)
         print(ppk)
         status = self.valid_type(report_select)
@@ -206,10 +206,10 @@ class ReportIndividualCompanyOrDriver(View):
         company = model.objects.filter(pk=ppk)
         print(company)
 
-        return status, company,report_select
+        return status, company, report_select
 
     def get(self, *args, **kwargs):
-        status, data,_type = self.set_data()
+        status, data, _type = self.set_data()
         if status == False:
             return alert("Algo Esta Haciendo Mal Que No Se Pudo Generar El PDF")
         if (data) == None:
@@ -220,7 +220,7 @@ class ReportIndividualCompanyOrDriver(View):
         pdf.add_page()
 
         pdf.set_font('Arial', 'B', 14)
-        name_title = "DE LA EMPRESA " if _type =="indiviual_company" else 'DEL CONDUCTOR'
+        name_title = "DE LA EMPRESA " if _type == "indiviual_company" else 'DEL CONDUCTOR'
         pdf.cell(0, 0, f'INFORMACIÓN {name_title}', 0, 1, 'C')
         pdf.set_font('Arial', 'B', 9)
         pdf.ln(10)
@@ -231,40 +231,42 @@ class ReportIndividualCompanyOrDriver(View):
             for i in data:
 
                 pdf.cell(30, 10, 'DOCUMENTO', 1, 0, 'C')
-                pdf.cell(155,10,"TIPO DE COMAPAÑIA",1,1,"C")
-                pdf.cell(30,10,i.get_document(),1,0,"C")
-                pdf.cell(155,10,i.type_company.name.upper(),1,1,"C")
-                pdf.cell(185,10,"NOMBRE",1,1,"C") if _type == "indiviual_company" else pdf.cell(155,10,"NOMBRE",1,1,"C")
-                pdf.cell(185,10,i.get_full_name(),1,1,"C")
+                pdf.cell(155, 10, "TIPO DE COMAPAÑIA", 1, 1, "C")
+                pdf.cell(30, 10, i.get_document(), 1, 0, "C")
+                pdf.cell(155, 10, i.type_company.name.upper(), 1, 1, "C")
+                pdf.cell(185, 10, "NOMBRE", 1, 1, "C") if _type == "indiviual_company" else pdf.cell(
+                    155, 10, "NOMBRE", 1, 1, "C")
+                pdf.cell(185, 10, i.get_full_name(), 1, 1, "C")
 
                 pdf.cell(92.5, 10, 'TELEFONO MOVIL', 1, 0, 'C')
                 pdf.cell(92.5, 10, 'TELEFONO DE CASA', 1, 1, 'C')
                 if i.tlf == None:
-                    pdf.cell(92.5,10,"NO POSEE",1,1,"C")
+                    pdf.cell(92.5, 10, "NO POSEE", 1, 1, "C")
                 else:
-                    pdf.cell(92.5,10,(i.tlf),1,0,"C")
+                    pdf.cell(92.5, 10, (i.tlf), 1, 0, "C")
 
                 if i.tlf_house == None:
-                    pdf.cell(92.5,10,"NO POSEE",1,1,"C")
+                    pdf.cell(92.5, 10, "NO POSEE", 1, 1, "C")
                 else:
-                    pdf.cell(92.5,10,(i.tlf_house),1,1,"C")
-                
+                    pdf.cell(92.5, 10, (i.tlf_house), 1, 1, "C")
+
                 pdf.cell(61.6, 10, 'ESTADO', 1, 0, 'C')
                 pdf.cell(61.6, 10, 'MUNICIPIO', 1, 0, 'C')
                 pdf.cell(61.8, 10, 'PARROQUIA', 1, 1, 'C')
 
-                pdf.cell(61.6,10,(i.state.name.upper()),1,0,"C")
-                pdf.cell(61.6,10,(i.municipality.name.upper()),1,0,"C")
-                pdf.cell(61.8,10,(i.parish.name.upper()),1,1,"C")
+                pdf.cell(61.6, 10, (i.state.name.upper()), 1, 0, "C")
+                pdf.cell(61.6, 10, (i.municipality.name.upper()), 1, 0, "C")
+                pdf.cell(61.8, 10, (i.parish.name.upper()), 1, 1, "C")
 
-                pdf.cell(185,10,"DIRECCIÓN",1,1,"C")
-                pdf.multi_cell(185,5,i.address,1,"J")
+                pdf.cell(185, 10, "DIRECCIÓN", 1, 1, "C")
+                pdf.multi_cell(185, 5, i.address, 1, "J")
 
                 pdf.ln(10)
                 pdf.set_font('Arial', 'B', 14)
 
-                name_second_title = "A LA EMPRESA " if _type =="indiviual_company" else 'DEL CONDUCTOR'
-                pdf.cell(0, 0, f'INSPECCIONES REALIZADAS {name_second_title}', 0, 1, 'C')
+                name_second_title = "A LA EMPRESA " if _type == "indiviual_company" else 'DEL CONDUCTOR'
+                pdf.cell(
+                    0, 0, f'INSPECCIONES REALIZADAS {name_second_title}', 0, 1, 'C')
                 pdf.set_font('Arial', 'B', 9)
 
                 pdf.ln(10)
@@ -274,9 +276,9 @@ class ReportIndividualCompanyOrDriver(View):
                 pdf.cell(45, 8, 'FECHA DE INSPECCIÓN', 1, 0, 'C')
                 pdf.cell(40, 8, 'RESULTADO', 1, 0, 'C')
                 pdf.cell(90, 8, 'REALIZADO POR ', 1, 1, 'C')
-                
+
                 for x in i.get_inspections():
-                    
+
                     _date = x.date.strftime('%d/%m/%Y')
                     status_result = x.result
                     result = 'Muy Bueno' if status_result == "is_verygood" else 'Bueno' if status_result == "is_good" else 'Malo'
@@ -284,36 +286,38 @@ class ReportIndividualCompanyOrDriver(View):
                     pdf.cell(10, 8, str(cont), 1, 0, 'C')
                     pdf.cell(45, 8, _date, 1, 0, 'C')
                     pdf.cell(40, 8, result, 1, 0, 'C')
-                    pdf.cell(90, 8, (x.account_register.get_full_name()), 1, 0, 'C')
+                    pdf.cell(
+                        90, 8, (x.account_register.get_full_name()), 1, 0, 'C')
 
-                    cont+=1
+                    cont += 1
         else:
             for i in data:
                 pdf.cell(30, 10, 'DOCUMENTO', 1, 0, 'C')
-                pdf.cell(155,10,"NOMBRE",1,1,"C")
-                pdf.cell(30,10,i.get_document(),1,0,"C")
-                pdf.cell(155,10,i.get_full_name(),1,1,"C")
+                pdf.cell(155, 10, "NOMBRE", 1, 1, "C")
+                pdf.cell(30, 10, i.get_document(), 1, 0, "C")
+                pdf.cell(155, 10, i.get_full_name(), 1, 1, "C")
 
                 pdf.cell(92.5, 10, 'TELEFONO MOVIL', 1, 0, 'C')
                 pdf.cell(92.5, 10, 'TELEFONO DE CASA', 1, 1, 'C')
                 if i.tlf == None:
-                    pdf.cell(92.5,10,"NO POSEE",1,1,"C")
+                    pdf.cell(92.5, 10, "NO POSEE", 1, 1, "C")
                 else:
-                    pdf.cell(92.5,10,(i.tlf),1,0,"C")
+                    pdf.cell(92.5, 10, (i.tlf), 1, 0, "C")
 
                 if i.tlf_house == None:
-                    pdf.cell(92.5,10,"NO POSEE",1,1,"C")
+                    pdf.cell(92.5, 10, "NO POSEE", 1, 1, "C")
                 else:
-                    pdf.cell(92.5,10,(i.tlf_house),1,1,"C")
-                
-                pdf.cell(185,10,"DIRECCIÓN",1,1,"C")
-                pdf.multi_cell(185,5,i.address,1,"J")
+                    pdf.cell(92.5, 10, (i.tlf_house), 1, 1, "C")
+
+                pdf.cell(185, 10, "DIRECCIÓN", 1, 1, "C")
+                pdf.multi_cell(185, 5, i.address, 1, "J")
 
                 pdf.ln(10)
                 pdf.set_font('Arial', 'B', 14)
 
-                name_second_title = "A LA EMPRESA " if _type =="indiviual_company" else 'DEL CONDUCTOR'
-                pdf.cell(0, 0, f'INSPECCIONES REALIZADAS {name_second_title}', 0, 1, 'C')
+                name_second_title = "A LA EMPRESA " if _type == "indiviual_company" else 'DEL CONDUCTOR'
+                pdf.cell(
+                    0, 0, f'INSPECCIONES REALIZADAS {name_second_title}', 0, 1, 'C')
                 pdf.set_font('Arial', 'B', 9)
 
                 pdf.ln(10)
@@ -333,9 +337,10 @@ class ReportIndividualCompanyOrDriver(View):
                     pdf.cell(10, 8, str(cont), 1, 0, 'C')
                     pdf.cell(45, 8, _date, 1, 0, 'C')
                     pdf.cell(40, 8, result, 1, 0, 'C')
-                    pdf.cell(90, 8, (x.account_register.get_full_name()), 1, 0, 'C')
+                    pdf.cell(
+                        90, 8, (x.account_register.get_full_name()), 1, 0, 'C')
 
-                    cont+=1
+                    cont += 1
 
             pdf.ln()
 
@@ -349,7 +354,7 @@ class ReportIndividualCompanyOrDriver(View):
 
 
 class ReportInspectionGeneralView(View):
-    
+
     def get(self, request, *args, **kwargs):
         type_company = request.GET.get('type_company', '')
         company = request.GET.get('company', '')
@@ -367,12 +372,11 @@ class ReportInspectionGeneralView(View):
 
         if type_company != '':
             data = get_type_company_report(type_company, date, week1, week2)
-            
 
         if company != '':
             data = get_company_report(company, date, week1, week2)
 
-        self.form(data)        
+        self.form(data)
         fs = FileSystemStorage()
         with fs.open(FILENAME) as pdf:
             response = HttpResponse(pdf)
@@ -387,10 +391,46 @@ class ReportInspectionGeneralView(View):
         pdf.set_font('Arial', 'B', 14)
         pdf.cell(0, 0, f'Reporte de Inspecciones', 0, 1, 'C')
         pdf.ln(10)
-        
-        pdf.set_font('Arial', '', 12)
-        pdf.cell(185, 7, f'Tipo De Compañia: Test_tiles', 1, 1,)
-        pdf.cell(10, 7, f'', 0, 0,)
-        pdf.cell(185, 7, f'Nombre De la Compañia: Test_tiles_Company', 1, 1,)
+
+        for key, ins in enumerate(data):
+            print(ins)
+            index_ins = key + 1
+            pdf.set_font('Arial', 'B', 12)
+            pdf.cell(185, 10,
+                     f'{index_ins}) Tipo De Compañia: {ins["type_company"].upper()}', 1, 1,)
+            if len(ins['companys']) != 0:
+                for key_company, company in enumerate(ins['companys']):
+                    pdf.set_font('Arial', 'B', 12)
+                    pdf.cell(185, 10,
+                             f'  {index_ins}.{key_company+1}) Nombre De la Compañia: {company["name"].upper()}', 1, 1,)
+
+                    pdf.set_font('Arial', 'B', 11)
+                    pdf.cell(10, 7, '#', 1, 0, 'C')
+                    pdf.cell(45, 7, 'Fecha De Inspección', 1, 0, 'C')
+                    pdf.cell(45, 7, 'Proxima Inspección', 1, 0, 'C')
+                    pdf.cell(35, 7, 'Resultado', 1, 0, 'C')
+                    pdf.cell(50, 7, 'Observación', 1, 1, 'C')
+
+                    if len(company['inspections']) != 0:
+                        for key_inspections, inspection in enumerate(company['inspections']):
+                            info = inspection['fields']
+                            result = 'Muy Bueno' if info['result'] == "is_verygood" else 'Bueno' if info[
+                                'result'] == "verygood" else 'Malo'
+                            pdf.set_font('Arial', '', 11)
+                            pdf.cell(10, 7, f'{key_inspections+1}', 1, 0, 'C')
+                            pdf.cell(45, 7, f'{info["date"]}', 1, 0, 'C')
+                            pdf.cell(45, 7, f'{info["next_date"]}', 1, 0, 'C')
+                            pdf.cell(35, 7, f'{result}', 1, 0, 'C')
+                            pdf.set_font('Arial', '', 9)
+                            pdf.multi_cell(
+                                50, 7, f'{info["notes"]}', 1, 1, 'C')
+                    else:
+                        pdf.set_font('Arial', 'B', 12)
+                        pdf.cell(185, 10,
+                                 f'No Tiene Inspecciones Registradas', 1, 1, 'C')
+            else:
+                pdf.set_font('Arial', 'B', 12)
+                pdf.cell(185, 10, f'No Tiene Compañias Registradas', 1, 1, 'C')
+            pdf.ln(10)
 
         pdf.output(FILENAME, 'F')
